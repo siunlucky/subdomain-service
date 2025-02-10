5be44248c8efc6ca0f4f22ac4e2ab6b9e49c9339
george ibrahim hanif <122005621+JorLojor@users.noreply.github.com>
2025-02-10 17:53:34 +0700

    initial BE setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
